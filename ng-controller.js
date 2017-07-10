e5453d5dc7344edef685a87aed85055bede73646
sakda homhuan <sakda.homhuan@gmail.com>
2017-07-10 10:36:20 +0700

    add fix bug

--- ng-controller.js
+++ ng-controller.js
@@ -102,7 +102,7 @@ angular.module('app.controller', ['ui-leaflet'])
                 lat: pos.lat,
                 lng: pos.lng,
                 focus: true,
-                message: "สังเกตุ, ถ้ามีกลุ่มเมฆในวงกลมแดง</br>แสดงว่ามีฝนใกล้ๆ บ้านเรา (20km)",
+                message: "สังเกตุ!! ถ้ามีกลุ่มเมฆในวงกลมแดง</br>แสดงว่ามีฝนใกล้ๆ บ้านเรา (20km)",
                 draggable: false
             };
         var circleMarker = {
